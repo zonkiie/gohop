166fb76b47f8e5e848fe615130dbd95a173fe7c6
Rainer Datzer <rdatz@gmx.de>
2018-04-08 04:39:48 +0200

    working on pluginloader.go

--- pluginloader.go
+++ pluginloader.go
@@ -8,8 +8,19 @@ import (
 var (
 )
 
-func loadPlugin(filename string) {
+func loadPlugin(filename string, entry_func ...string) {
 	if _, s_err := os.Stat(filename); !os.IsNotExist(s_err) {
 		p, p_err := plugin.Open(filename)
+		if p_err != nil {
+			panic(p_err)
+		}
+		startfuncname := "startfunc"
+		if len(entry_func) > 0 {
+			startfuncname = entry_func[0]
+		}
+		startfunc, l_err := p.Lookup(startfuncname)
+		if l_err != nil {
+			panic(l_err)
+		}
 	}
 }
